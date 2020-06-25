500b25e193b961bd81b18aecc3bfd9fa2385a54e
Grieco <grieco@intre.it>
2020-06-25 17:01:29 +0200

    Fix scripts

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -18,41 +18,41 @@
 
 import {FastifyInstance} from "fastify"
 
-const lc39 = require('@mia-platform/lc39')
-
 export interface ProcessEnv {
-    [key: string]: string | undefined
+  [key: string]: string | undefined
 }
-
-let fastify: FastifyInstance
-
-async function setupFastify(envVariables: ProcessEnv): Promise<FastifyInstance> {
-    const fastify = await lc39('./dist/index.js', {
-        logLevel: 'silent',
-        envVariables,
-    })
-    return fastify
-}
-
-beforeAll(async () => {
-    fastify = await setupFastify({
-        USERID_HEADER_KEY: 'userid',
-        GROUPS_HEADER_KEY: 'groups',
-        CLIENTTYPE_HEADER_KEY: 'clienttype',
-        BACKOFFICE_HEADER_KEY: 'backoffice',
-        MICROSERVICE_GATEWAY_SERVICE_NAME: 'microservice-gateway.example.org',
-    })
-});
-
-afterAll(async () => {
-    await fastify.close()
-})
 
 describe('mia_template_service_name_placeholder', () => {
-    /*
-    * Insert your tests here.
-    */
-    test('Insert test name here', () => {
+  const lc39 = require('@mia-platform/lc39')
 
+  let fastify: FastifyInstance
+
+  async function setupFastify(envVariables: ProcessEnv): Promise<FastifyInstance> {
+    const fastify = await lc39('./dist/index.js', {
+      logLevel: 'silent',
+      envVariables,
     })
+    return fastify
+  }
+
+  beforeAll(async () => {
+    fastify = await setupFastify({
+      USERID_HEADER_KEY: 'userid',
+      GROUPS_HEADER_KEY: 'groups',
+      CLIENTTYPE_HEADER_KEY: 'clienttype',
+      BACKOFFICE_HEADER_KEY: 'backoffice',
+      MICROSERVICE_GATEWAY_SERVICE_NAME: 'microservice-gateway.example.org',
+    })
+  });
+
+  afterAll(async () => {
+    await fastify.close()
+  })
+
+  /*
+  * Insert your tests here.
+  */
+  test('Insert test name here', () => {
+
+  })
 })
